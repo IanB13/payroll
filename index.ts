4c27dfb85df7a93a48d562294d40404b62efc7a0
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-11-24 17:12:20 +0000

    Added linter

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 import printPayDates from "./printPayDates";
 
 if(process.argv.length === 2){
-    printPayDates(new Date(Date.now()))
+    printPayDates(new Date(Date.now()));
 }
 else if(process.argv.length === 3){
-    const currentDate = process.argv[2]
+    const currentDate = process.argv[2];
     if(Date.parse(currentDate)){
-        printPayDates(new Date(currentDate))
+        printPayDates(new Date(currentDate));
     }
     else{
-        console.error("invalid date provided")
+        console.error("invalid date provided");
     }
 }
 else{
-    console.error("too many arguments provided")
+    console.error("too many arguments provided");
 }
--- payDate.ts
+++ payDate.ts
@@ -1,14 +1,14 @@
-const payDate = (year: number, month: number, type: "base" | "bonus"):Date => {
+const payDate = (year: number, month: number, payType: "base" | "bonus"):Date => {
     let payDate = null;
 
     /*   bonuses are paid on the 15 if not a weekend, if it is a weekend then
         bonus is paid on the following Wednesday.  */
-    if (type === "bonus") {
+    if (payType === "bonus") {
         const midMonth = new Date(year, month, 15);
         const midMonthDay = midMonth.getDay();
         if(midMonthDay === 0 || midMonthDay === 6){
-            const plusDays = midMonthDay?4:3
-            payDate = new Date(year, month, 15 + plusDays) 
+            const plusDays = midMonthDay?4:3;
+            payDate = new Date(year, month, 15 + plusDays); 
         } 
         else{
             payDate = midMonth;
@@ -16,22 +16,23 @@ const payDate = (year: number, month: number, type: "base" | "bonus"):Date => {
     }
     /*   base salary is paid at the last day of the month if not a weekend. if it 
     is a weekend then base salary is paid on the Friday before the weekend.  */
-    else if (type === "base") {
+    else if (payType === "base") {
         const monthEnd = new Date(year, month + 1, 0);
         const monthEndDay = monthEnd.getDay();
 
         // 0 is Sunday, 6 is Saturday
         if(monthEndDay === 0 || monthEndDay === 6){
-            const minusDays = monthEndDay?1:2
-            payDate = new Date(year, month +1, 0 - minusDays) 
+            const minusDays = monthEndDay?1:2;
+            payDate = new Date(year, month +1, 0 - minusDays); 
         } 
         else{
-            payDate = monthEnd
+            payDate = monthEnd;
         }
 
     }
+    if(payDate === null) throw new Error("No date created, likely a problem with pay type");
 
-    return payDate
-}
+    return payDate;
+};
 
 export default payDate;
